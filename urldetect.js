9f35dc09f326e2a57ce21ee1f3fef1d7297dcbe2
E-Tech Developers <106537091+E-TechDev@users.noreply.github.com>
2023-04-12 23:50:26 +0100

    Update urldetect.js

--- urldetect.js
+++ urldetect.js
@@ -1,15 +1,18 @@
 var text = document.getElementById("your-div").innerHTML;  
-
+// Check if it starts with an http. Stop on a space bar 
  let rep = text.match(/(?:^|[ ])http([^\s]+)/gi);
 for(let i = 0; i < rep.length;  
 i++) { 
-console.log(rep[i]); 
+console.log(rep[i]);  
 let pos = rep[i]; 
+// Check the last character of the detected strings
 var check = pos.slice(-1); 
 console.log(check); 
+// Check if the last character is a period (.)  
 if(check == ".") { 
 var lastIndex = pos.lastIndexOf('.');
-var replacement = "";
+var replacement = ""; 
+// Get rid of the period 
 var link =  pos.slice(0, lastIndex) + replacement + pos.slice(lastIndex + 1);
 console.log(link); 
 } else { 
