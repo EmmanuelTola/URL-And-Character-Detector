146d75458db302a7176cd8d713b21073c2c09fb6
Emmanuel Tola <106537091+EmmanuelTola@users.noreply.github.com>
2023-09-24 14:37:54 +0100

    Update detectchar.js

--- detectchar.js
+++ detectchar.js
@@ -1,3 +1,4 @@
+// @ Detect
 var text = document.getElementById("your-div").innerHTML;  
 // Select every word that begins with an @. Can be used for # too 
 let rel = text.match(/(?:^|[ ])@([a-zA-Z0-9]+)/gi);
@@ -14,3 +15,19 @@ var re = document.getElementById("your-div");
 re.innerHTML = re.innerHTML.replace(rel[i], "<a style='text-decoration: none; color: purple; font-weight: bold;' href='"+url+post+"'>"+rel[i]+"</a>"); 
  } 
  }
+
+ // Hashtag Detect 
+  let bel = text.match(/(?:^|[ ])#([a-zA-Z0-9]+)/gi); 
+   
+  if(bel) {
+for(let i = 0; i < bel.length;  
+i++) { 
+console.log(bel[i]); 
+var urls = "https://";
+let posts = bel[i].split(" @").join(""); 
+
+var re = document.getElementById("text");
+re.innerHTML = re.innerHTML.replace(bel[i], "<a style='text-decoration: none; color: purple; font-weight: bold;' href='"+urls+posts+"'>"+bel[i]+"</a>"); 
+ } 
+ } 
+  
